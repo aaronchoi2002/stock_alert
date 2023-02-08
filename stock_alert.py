dace24cbfc16dd424d267ac46110c43fe27bc1d5
aaronchoi2002 <112890250+aaronchoi2002@users.noreply.github.com>
2023-02-08 12:10:16 +0800

    Update stock_alert.py
    V_04

--- stock_alert.py
+++ stock_alert.py
@@ -14,38 +14,45 @@ st.header("Stock alert system_v03")
 
 
 def show_grid(newline):
-    df = pd.read_csv("Barry_stock_list_2023.csv")
+    df = pd.read_csv("Barry_stock_list_2023.csv", index_col=False)
     df = df.fillna("0")
-    df = df.set_index('Symbol')
 
-    tickers = df.index.to_list()
-    price = yf.download(tickers, period="5d", interval="15m")["Adj Close"].T
-    price = price.iloc[:,-1] # get the latest price
-    df["price"] = round(price,2)
-    df.reset_index(inplace=True)
+
     
     if newline == 'yes':
         ticker = st.sidebar.text_input('Add new ticker')
-        alert = st.sidebar.text_input('Add new alert')
+        alert = st.sidebar.number_input('Add new alert')
         remark = st.sidebar.text_input('Add new remark')
         data = [[ticker, alert, remark,""]]
         df_empty = pd.DataFrame(data, columns=["Symbol", "Alert", "Remark","price"])
         df = pd.concat([df, df_empty], axis=0, ignore_index=True)
-
+        
     gb = GridOptionsBuilder.from_dataframe(df)
-    gb.configure_default_column(editable=True)
+    gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=True)
+    gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren=True, groupSelectsFiltered=True)
+    gb.configure_pagination(enabled=True)
     grid_table = AgGrid(
         df,
         height=300,
         gridOptions=gb.build(),
+        reload_data=False,
         fit_columns_on_grid_load=True,
+        update_mode=GridUpdateMode.MODEL_CHANGED,
         allow_unsafe_jscode=True,
         )
     return grid_table
 
     
 def update(grid_table):
+    # df = df.set_index('Symbol')
     grid_table_df = pd.DataFrame(grid_table['data'])
+    grid_table_df = grid_table_df.set_index('Symbol')
+    tickers = grid_table_df.index.to_list()
+    price = yf.download(tickers, period="5d")["Adj Close"].T
+    price = price.iloc[:,-1] # get the latest price
+    grid_table_df["price"] = round(price,2)
+    grid_table_df.reset_index(inplace=True)
+    # grid_table_df = pd.DataFrame(grid_table['data'])
     grid_table_df.to_csv('Barry_stock_list_2023.csv', index=False)    
      
 
@@ -53,10 +60,12 @@ def alert(grid_table):
     for index,row in grid_table.iterrows():
         if row.Alert > row.price:
             st.code(f"{row.Symbol} target price: {row.Alert} reached ")
+            
+    
 
 # start
 
-tab1, tab2 = st.tabs(["Result", "Data"])
+tab1, tab2 = st.tabs(["Alert", "Data"])
 with tab1:
     addline = st.sidebar.radio('Add New Stock', options=['yes', 'no'], index=1, horizontal=True)
     grid_table = show_grid(addline)
@@ -64,6 +73,7 @@ with tab1:
     grid_table_df = pd.DataFrame(grid_table['data'])   
     st.markdown("---")  
     alert(grid_table_df)
+
     
 with tab2:
     grid_table_df
